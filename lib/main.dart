f8fa904696b8c5cac9159686e3163b67402d9fdd
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-02-21 08:50:11 +0300

    Feat:Bug fixes
    The code could not work because I did not use the get pages method,so I had to define the list of routes in the main file

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'dart:collection';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:myapp_flutter/pages/login.dart';
+import 'package:myapp_flutter/utils/routes.dart';
 
 void main() {
   runApp(GetMaterialApp(
-    initialRoute: "/", // Change the initial route to /home
+    initialRoute: "/", // Change the initial route to /login
     debugShowCheckedModeBanner: false,
+    getPages: Routes.routes,
   ));
 }
 
