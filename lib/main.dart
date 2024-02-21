3e024addc14d8de530aad73ecbd6dccbd0199fd4
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-02-21 08:44:02 +0300

    Feat:Added GetMaterialApp
    Purpose:Using the Get method to manage all theroutes and all

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'dart:collection';
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:myapp_flutter/pages/login.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(GetMaterialApp(
     home: Login(),
     debugShowCheckedModeBanner: false,
   ));
