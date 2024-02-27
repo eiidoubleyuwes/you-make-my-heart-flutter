a43c3db2f325bfeb5e63b011cd73bb9517d29e82
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-02-27 20:38:37 +0300

    Feat:Changed the code
    This is kind of a defeat because I want to accomodate the images

--- lib/views/Racecards.dart
+++ lib/views/Racecards.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myapp_flutter/pages/TrackDetailsPage.dart';
 import 'package:myapp_flutter/pages/calender.dart';
 
 class RaceCard extends StatelessWidget {
@@ -45,36 +46,6 @@ class RaceCard extends StatelessWidget {
                   Text(race.time),
                 ],
               ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-class TrackDetailsPage extends StatelessWidget {
-  final F1Race race;
-
-  const TrackDetailsPage({Key? key, required this.race}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(race.name),
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              // Display track image (implementation needed)
-              Text(
-                "Track Information",
-                style: Theme.of(context).textTheme.headline6,
-              ),
-              const SizedBox(height: 10.0),
-              // Display track details (circuit length, layout image, history, etc.) (implementation needed)
             ],
           ),
         ),
