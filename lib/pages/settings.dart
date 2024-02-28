f24ecaf6e323b8334172d66088f3d16c827aa8a4
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-02-28 08:28:21 +0300

    DEFEAT:Button does not work
    This makes me sad bcause it won't even show up

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:myapp_flutter/configs/constants.dart';
+import 'package:myapp_flutter/views/custombutton.dart';
 import 'package:myapp_flutter/views/customtexts.dart';
 
 class SettingsPage extends StatefulWidget {
@@ -10,51 +12,54 @@ class SettingsPage extends StatefulWidget {
 class _SettingsPageState extends State<SettingsPage> {
   bool _darkModeEnabled = false;
   bool _notificationsEnabled = true;
-  double _fontSize = 16.0;
 
   final damn = const Color(0xFFEC0023); // F1 Red
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-        padding: EdgeInsets.all(16.0),
-        itemCount: 2, // Assuming only two SwitchListTile items
-        itemBuilder: (context, index) {
-          switch (index) {
-            case 0:
-              return SwitchListTile(
-                title: customText(
-                  "Dark Mode", // Changed "Dark Mode" to "Dark Mode
-                  label: 'Dark Mode',
-                  fontWeight: FontWeight.bold,
-                  color: damn,
-                ),
-                value: _darkModeEnabled,
-                onChanged: (value) {
-                  setState(() {
-                    _darkModeEnabled = value;
-                  });
-                },
-              );
-            case 1:
-              return SwitchListTile(
-                title: customText(
-                  "Notifications", // Changed "Notifications" to "Notifications
-                  label: 'Notifications',
-                  fontWeight: FontWeight.bold,
-                  color: damn,
-                ),
-                value: _notificationsEnabled,
-                onChanged: (value) {
-                  setState(() {
-                    _notificationsEnabled = value;
-                  });
-                },
-              );
-            default:
-              return SizedBox(); // Handle potential out-of-bounds index
-          }
-        },
-      );
+      padding: EdgeInsets.all(16.0),
+      itemCount: 2, // Assuming only two SwitchListTile items
+      itemBuilder: (context, index) {
+        switch (index) {
+          case 0:
+            return SwitchListTile(
+              title: customText(
+                "Dark Mode", // Changed "Dark Mode" to "Dark Mode
+                label: 'Dark Mode',
+                fontWeight: FontWeight.bold,
+                color: damn,
+              ),
+              value: _darkModeEnabled,
+              onChanged: (value) {
+                setState(() {
+                  _darkModeEnabled = value;
+                });
+              },
+            );
+          case 1:
+            return SwitchListTile(
+              title: customText(
+                "Notifications", // Changed "Notifications" to "Notifications
+                label: 'Notifications',
+                fontWeight: FontWeight.bold,
+                color: damn,
+              ),
+              value: _notificationsEnabled,
+              onChanged: (value) {
+                setState(() {
+                  _notificationsEnabled = value;
+                });
+              },
+            );
+          //Custom button to route back to the login page once clicked
+          case 2:
+            return custombutton(
+                label: 'Log out', action: () => Get.toNamed("/login"));
+          default:
+            return const SizedBox(); // Handle potential out-of-bounds index
+        }
+      },
+    );
   }
 }
