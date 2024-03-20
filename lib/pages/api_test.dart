b172add8f58ec44f1ede1d561845dd82c1d810ad
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-03-20 10:36:23 +0300

    DEFEAT:Code will not display student data
    I must get to the bottom of this,however its all to do with state management

--- lib/pages/api_test.dart
+++ lib/pages/api_test.dart
@@ -1,5 +1,12 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_instance/get_instance.dart';
 import 'package:http/http.dart' as http;
+import 'package:myapp_flutter/controllers/studentcontroller.dart';
+import 'package:myapp_flutter/models/studentmodel.dart';
+
+StudentController studentController = Get.put(StudentController());
 
 class Api_test extends StatelessWidget {
   const Api_test({super.key});
@@ -10,27 +17,46 @@ class Api_test extends StatelessWidget {
       appBar: AppBar(
         title: Text('API Test'),
       ),
-      body: Column(
-        children: [
-          ElevatedButton(
-            onPressed: () {
-              getAPI();
-            },
-            child: Text('Get Data'),
-          ),
-        ],
+      body: Obx(
+        () => Column(
+          children: [
+            ElevatedButton(onPressed: getAPI, child: Text('Get Data')),
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: studentController.studentList.length,
+              itemBuilder: (context, index) {
+                return Row(children: [
+                  Text('${studentController.studentList[index].sname}'),
+                  Text('${studentController.studentList[index].admissionumb}'),
+                ]);
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
+
 //Read data from API
   Future<void>? getAPI() async {
-    http.Response response;
-    //converts the URL to URI
-    response = await http.get(Uri.parse("http://barakambuguaon.top/student/read.php"));
-    if (response.statusCode == 200) {
-      print(response.body);
-    } else {
-      print('Failed to load data ${response.statusCode}');
+    try {
+      http.Response response;
+      //converts the URL to URI
+      response = await http
+          .get(Uri.parse("http://barakambuguaon.top/student/read.php"));
+      if (response.statusCode == 200) {
+        var studentResponse = json.decode(response.body);
+        var studentDataResponse = studentResponse['data'];
+        var newlist = studentDataResponse
+            .map((students) => studentModel.fromJson(students))
+            .toList();
+        studentController.updateStudentlist(newlist);
+      } else {
+        print("Failed to get data {response.statusCode}");
+      }
+      studentController.updateloadingstudentdata(false);
+    } catch (e) {
+      print(e.toString());
     }
   }
 }
