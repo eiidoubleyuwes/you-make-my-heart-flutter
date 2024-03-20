3222c90f4f74b7d737c52e087c5de1192dbd4174
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-03-20 10:41:07 +0300

    Feat:Added ID
    This works because I am cool

--- lib/models/studentmodel.dart
+++ lib/models/studentmodel.dart
@@ -1,13 +1,15 @@
 class studentModel{
   final String sname;
   final String admissionum;
+  final dynamic id;
  //Constructor
-  studentModel({required this.sname, required this.admissionum});
+  studentModel({required this.sname, required this.admissionum, required this.id});
   //Special constructor to convert json to object
   factory studentModel.fromJson(Map<String, dynamic> json) {
     return studentModel(
-      sname: json['sname'],
-      admissionum: json['admissionum'],
+      sname: json['sname']??'',
+      admissionum: json['admissionum']??'',
+      id: json['id']??'',
     );
   }
 }
