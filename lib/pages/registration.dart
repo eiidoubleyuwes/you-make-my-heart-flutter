a24e14c2f6204c07a7fd738f5f03a8374914eed1
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-04-25 16:57:16 +0300

    Feat:Dealing with many changes
    Added a snack error bar
    added the registration logic
    added the simple

--- lib/pages/registration.dart
+++ lib/pages/registration.dart
@@ -1,18 +1,26 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:myapp_flutter/configs/constants.dart';
 import 'package:myapp_flutter/views/custombutton.dart';
 import 'package:myapp_flutter/views/customcontroller.dart';
 import 'package:myapp_flutter/views/customtexts.dart';
+import 'package:http/http.dart' as http;
 
 TextEditingController username = TextEditingController();
 TextEditingController password = TextEditingController();
+TextEditingController confpassword = TextEditingController();
+TextEditingController email = TextEditingController();
+
 class RegistrationPage extends StatelessWidget {
+  const RegistrationPage({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        automaticallyImplyLeading: true,),
+        automaticallyImplyLeading: true,
+      ),
       body: SingleChildScrollView(
         child: Center(
           child: Padding(
@@ -45,6 +53,20 @@ class RegistrationPage extends StatelessWidget {
                 const SizedBox(
                   height: 10.0,
                 ),
+                const customText(
+                  "Email",
+                  label: 'Email',
+                  fontSize: FontSize(18.0),
+                ),
+                CustomTextWidget(
+                  hint: "Email",
+                  icon: Icons.email,
+                  controller: email,
+                  ispassword: false,
+                ),
+                const SizedBox(
+                  height: 10.0,
+                ),
 
                 const customText(
                   "Password",
@@ -65,10 +87,10 @@ class RegistrationPage extends StatelessWidget {
                   label: 'Retype Password',
                   fontSize: FontSize(18.0),
                 ),
-                 CustomTextWidget(
-                  hint: "Password",
+                CustomTextWidget(
+                  hint: "Retype Password",
                   icon: Icons.lock,
-                  controller: password,
+                  controller: confpassword,
                   ispassword: true,
                 ),
                 const SizedBox(
@@ -79,7 +101,9 @@ class RegistrationPage extends StatelessWidget {
                 custombutton(
                   label: 'Register',
                   labelColor: appWhiteColor,
-                  action: () => Get.toNamed("/home"),
+                  action: () {
+                    register();
+                    }
                 ),
               ],
             ),
@@ -87,5 +111,39 @@ class RegistrationPage extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+Future<void> register() async {
+  var waitforsnakbar = false;
+  if (password.text.trim() == confpassword.text.trim() && username.text.isNotEmpty && email.text.isNotEmpty && password.text.isNotEmpty && confpassword.text.isNotEmpty && !waitforsnakbar) {
+    http.Response response;
+    response = await http.post(
+      Uri.parse("http://barakambuguaon.top/news/register.php"),
+      body: {
+        "username": username.text.trim(),
+        "email": email.text.trim(),
+        "password": password.text.trim(),
+      },
+    );
+    Get.snackbar("Passwords match", "Please procced to login again",
+        snackPosition: SnackPosition.BOTTOM,
+        backgroundColor: Colors.green,
+        colorText: Colors.white);
+        waitforsnakbar = true;
+    await Future.delayed(const Duration(seconds: 1));
+    waitforsnakbar = false;
+    Get.toNamed("/login");
+  } else if (password.text.trim() != confpassword.text.trim()){
+    Get.snackbar("Passwords don't match", "Please retype the password",
+        snackPosition: SnackPosition.BOTTOM,
+        backgroundColor: Colors.red,
+        colorText: Colors.white);
+  }
+  else {
+    Get.snackbar("Registration Failed", "Please fill in all the fields",
+        snackPosition: SnackPosition.BOTTOM,
+        backgroundColor: Colors.red,
+        colorText: Colors.white);
   }
 }
