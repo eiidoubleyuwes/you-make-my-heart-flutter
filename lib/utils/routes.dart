0de0d4240e241ea637271b2b4939bfac7c985f13
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-04-26 13:23:02 +0300

    Feat:Added relevant routes

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -1,6 +1,5 @@
 import 'package:get/get.dart';
 import 'package:myapp_flutter/pages/api_test.dart';
-import 'package:myapp_flutter/pages/calender.dart';
 import 'package:myapp_flutter/pages/dashboard.dart';
 import 'package:myapp_flutter/pages/login.dart';
 import 'package:myapp_flutter/pages/registration.dart';
@@ -11,7 +10,6 @@ class Routes {
     GetPage(name: "/", page: () => Login()),// First screen is represented by the / route meaning the root of the app.
     GetPage(name: "/registration", page: () => RegistrationPage()),
     GetPage(name: "/home", page: () => Dashboard()),
-    GetPage(name: "/Calender", page: () => CalendarPage()),
     GetPage(name: "/api_test", page: () => Api_test()),
     GetPage(name: "/settings", page: () => SettingsPage()),
   ];
