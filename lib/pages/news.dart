d10289ec41a194aca6a42425535ee5d6021cc7e7
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-04-26 11:34:03 +0300

    Feat:Reading data from the server

--- lib/pages/news.dart
+++ lib/pages/news.dart
@@ -1,38 +1,34 @@
-// ignore_for_file: camel_case_types
-
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:myapp_flutter/views/newscards.dart';
+import 'package:http/http.dart' as http;
 
-class newsPage extends StatelessWidget {
-  final List<NewsCard> news = [
-    NewsCard(
-      title: "Pierre Gasly Wins Dramatic Race in Spain",
-      subtitle: "Verstappen crashes out on final lap",
-      imageUrl: "assets/images/alpine.png",
-    ),
-    NewsCard(
-      title: "Leclerc Takes Pole Position in Monaco",
-      subtitle: "Sainz crashes in Q3",
-      imageUrl: "assets/images/logo-ferrari-18-.png",
-    ),
-    NewsCard(
-      title: "Hamilton Wins in Silverstone",
-      subtitle: "Verstappen frustrated after collision",
-      imageUrl: "assets/images/mercedes.jpg",
-    ),
-    NewsCard(
-      title: "Vettel Wins in Hungary",
-      subtitle: "First win for Him back at Red Bull",
-      imageUrl: "assets/images/red-bull.png",
-    ),
-     NewsCard(
-      title: "Mercedes distraught afther Hamilton exit",
-      subtitle: "Fred Vasseur: 'We were lucky'",
-      imageUrl: "assets/images/look.png",
-    ),
-  ];
-  newsPage({super.key});
+class newsPage extends StatefulWidget {
+  const newsPage({super.key});
 
+  @override
+  State<newsPage> createState() => _newsPageState();
+}
+
+class _newsPageState extends State<newsPage> {
+  late Future<List<NewsCard>> futureNewsCards;
+
+  @override
+  void initState() {
+    super.initState();
+    futureNewsCards = fetchNewsCards();
+  }
+static Future<List<NewsCard>> fetchNewsCards() async {
+    final response =
+        await http.get(Uri.parse('http://barakambuguaon.top/news/racedetails.php'));
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body) as List;
+      return data.map((item) => NewsCard.fromJson(item)).toList();
+    } else {
+      throw Exception(
+          'Failed to load news cards: Status code ${response.statusCode}');
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -40,11 +36,23 @@ class newsPage extends StatelessWidget {
         const SizedBox(height: 20.0),
         // F1 News Section
         Expanded(
-          child: ListView.builder(
-            scrollDirection: Axis.vertical,
-            itemCount: news.length,
-            itemBuilder: (context, index) {
-              return NewsCardWidget(news[index]);
+          child: FutureBuilder<List<NewsCard>>(
+            future: futureNewsCards,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                final newsCards = snapshot.data!;
+                return ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  itemCount: newsCards.length,
+                  itemBuilder: (context, index) {
+                    return NewsCardWidget(newsCards[index]);
+                  },
+                );
+              } else if (snapshot.hasError) {
+                return Center(child: Text('Error: ${snapshot.error}'));
+              }
+              // Display a loading indicator while waiting for data
+              return const Center(child: CircularProgressIndicator());
             },
           ),
         ),
